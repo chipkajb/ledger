1f236e5340fe39cb1eebf8ecbf778ebdab99832a
chipkajb <jbc274@cornell.edu>
2026-07-04 22:07:53 -0400

    trust host

--- src/auth.ts
+++ src/auth.ts
@@ -8,6 +8,7 @@ import { eq } from "drizzle-orm";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   ...authConfig,
+  trustHost: true,
   providers: [
     Credentials({
       name: "credentials",
